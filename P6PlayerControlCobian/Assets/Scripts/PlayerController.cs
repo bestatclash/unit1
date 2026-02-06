2f2ec37d0b5e53380dfd2f5ab17fbeeaf0ffa1ec
bestatclash <743376@student.dusd.net>
2026-02-06 13:51:58 -0800

    fineshed steps
    doing bonus steps

--- P6PlayerControlCobian/Assets/Scripts/PlayerController.cs
+++ P6PlayerControlCobian/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +17,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // we'll move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
