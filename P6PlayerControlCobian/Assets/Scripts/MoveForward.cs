74b6d038d9ce66d1f6ab1a9fd56a4ad7232a5734
bestatclash <743376@student.dusd.net>
2026-02-23 13:57:26 -0800

    doing plane
    almost finnished

--- P6PlayerControlCobian/Assets/Scripts/MoveForward.cs
+++ P6PlayerControlCobian/Assets/Scripts/MoveForward.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
-
 {
     public int speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
